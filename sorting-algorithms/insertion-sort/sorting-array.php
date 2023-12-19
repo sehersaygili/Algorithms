db337001a3bd02788d5c84f3c740ab66efd9ca03
Seher Saygılı <sehersygl111@gmail.com>
2023-12-19 22:51:06 +0300

    sorting-array

---
+++ sorting-algorithms/insertion-sort/sorting-array.php
@@ -0,0 +1,17 @@
+<?php
+
+function insertionSort(&$arr) {
+    $n = count($arr);
+    for ($i = 1; $i < $n; $i++) {
+        $key = $arr[$i];
+        $j = $i - 1;
+
+        while ($j >= 0 && $arr[$j] > $key) {
+            $arr[$j + 1] = $arr[$j];
+            $j = $j - 1;
+        }
+        $arr[$j + 1] = $key;
+    }
+}
+
+?>
